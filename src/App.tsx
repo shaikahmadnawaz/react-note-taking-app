c54f53d793d24cc6bcfe895b02b48f904b15e12d
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2022-11-19 21:13:19 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Hi</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
